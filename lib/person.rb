5b9a9b81ca78237f77f02be8dd8c394a46a07905
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-05-21 15:57:08 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
   def name=(person_name)
     @person_name = person_name
+  end
+  
+  def name
+    @person_name
+  end
+  
+  def job=(person_job)
+    @person_job = person_job
+  end
+  
+  def job 
+    @person_job
+  end
+  
+end  
